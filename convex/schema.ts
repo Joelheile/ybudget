0e4705536837e3735ee490080f90d6d56c61bd76
Joelheile <joel.heil-escobar@outlook.de>
2025-10-17 16:04:59 +0200

    created initial schema

---
+++ convex/schema.ts
@@ -0,0 +1,76 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    organizations: defineTable({
+        id: v.string(),
+        name: v.string(),
+    }),
+    users: defineTable({
+        id: v.string(),
+        firstName: v.string(),
+        lastName: v.string(),
+        email: v.string(),
+        organizationId: v.string(),
+        oauthId: v.string(),
+        oauthProvider: v.string(),
+        //role: 
+    }),
+    projects: defineTable({
+        id: v.string(),
+        name: v.string(),
+        parentId: v.string(),
+        organizationId: v.string(),
+        description: v.string(),
+        isActive: v.boolean()
+    }),
+    transactions: defineTable({
+        id: v.string(),
+        projectId: v.string(),
+        date: v.number(), //epoch timestamp
+        amount: v.number(),
+        reference: v.string(),
+        categoryId: v.string(),
+        donorId: v.string(),
+        isExpense: v.boolean(),
+        importedBy: v.string(),
+        importSource: v.union(
+            v.literal("sparkasse"),
+            v.literal("volksbank"),
+            v.literal("moss"),
+        )
+    }),
+    expectedTransactions: defineTable({
+        id: v.string(),
+        projectId: v.string(),
+        expectedDate: v.number(), //epoch timestamp
+        amount: v.number(),
+        reference: v.string(),
+        categoryId: v.string(),
+        donorId: v.string(),
+        isExpense: v.boolean(),
+        createdBy: v.string(),
+        matchedTransactionId: v.string(),
+        // -> could just take it if there is a matchedTransaction
+        // status: v.union( 
+        //     v.literal("open"),
+        //     v.literal("matched"),
+        // )
+    }),
+    categories: defineTable({
+       id: v.string(),
+       name: v.string(),
+       description: v.string(),
+       taxCostposition: v.number() // Kostenstelle 
+    }),
+    donors: defineTable({
+        id: v.string(),
+        name: v.string(),
+        type: v.union(
+            v.literal("donation"),
+            v.literal("non-profit"),
+            v.literal("sponsoring"),
+            v.literal("government"),
+        )
+    }),
+})
