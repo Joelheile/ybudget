f9e09c4cd0f5fb7f203737a92d87584d5a64fc0a
Joelheile <joel.heil-escobar@outlook.de>
2025-11-16 21:29:24 +0100

    chore: format 2

--- app/components/Sidebar/ProjectNav.tsx
+++ app/components/Sidebar/ProjectNav.tsx
@@ -149,7 +149,7 @@ function ProjectNavComponent({ id }: { id?: string }) {
                           )
                         )}
                       </SidebarMenuSub>
-                    </CollapsibleContent>
+                     </CollapsibleContent>
                   </>
                 ) : null}
               </SidebarMenuItem>
