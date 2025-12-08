99c888487d4da3cb2eb6b8ed0c5a9717030df8f7
Joelheile <joel.heil-escobar@outlook.de>
2025-12-08 20:48:50 +0100

    chore: split up user and query for easier reading

--- convex/organizations/organizations.test.ts
+++ convex/organizations/organizations.test.ts
@@ -7,14 +7,18 @@ import { modules, setupTestData } from "../test.setup";
 test("get organization name", async () => {
   const test = convexTest(schema, modules);
   const { userId } = await setupTestData(test);
-  const name = await test.withIdentity({ subject: userId }).query(api.organizations.queries.getOrganizationName, {});
+
+  const user = test.withIdentity({ subject: userId });
+  const name = await user.query(api.organizations.queries.getOrganizationName, {});
   expect(name).toBe("Test Organization");
 });
 
 test("get user organization", async () => {
   const test = convexTest(schema, modules);
   const { organizationId, userId } = await setupTestData(test);
-  const org = await test.withIdentity({ subject: userId }).query(api.organizations.queries.getUserOrganization, {});
+
+  const user = test.withIdentity({ subject: userId });
+  const org = await user.query(api.organizations.queries.getUserOrganization, {});
   expect(org?._id).toBe(organizationId);
   expect(org?.domain).toBe("test.com");
 });
@@ -22,15 +26,18 @@ test("get user organization", async () => {
 test("create organization", async () => {
   const test = convexTest(schema, modules);
   const userId = await test.run(async (ctx) => ctx.db.insert("users", { email: "new@test.com" }));
-  const orgId = await test.mutation(api.organizations.functions.createOrganization, {
+
+  const organizationId = await test.mutation(api.organizations.functions.createOrganization, {
     name: "New Org",
     domain: "test.com",
     userId,
   });
-  const org = await test.run(async (ctx) => ctx.db.get(orgId));
+
+  const org = await test.run(async (ctx) => ctx.db.get(organizationId));
   expect(org?.name).toBe("New Org");
+
   const projects = await test.run(async (ctx) =>
-    ctx.db.query("projects").withIndex("by_organization", (q) => q.eq("organizationId", orgId)).collect()
+    ctx.db.query("projects").withIndex("by_organization", (q) => q.eq("organizationId", organizationId)).collect()
   );
   expect(projects).toHaveLength(1);
   expect(projects[0].name).toBe("Rücklagen");
