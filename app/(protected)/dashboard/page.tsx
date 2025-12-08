c6ddf1694b46cfb40bf998f4958588993398298e
Joelheile <joel.heil-escobar@outlook.de>
2025-12-08 13:55:37 +0100

    chore: merged Dashboard into page and not seperate component

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,31 +1,49 @@
 "use client";
-import DashboardUI from "@/(protected)/dashboard/DashboardUI";
-import { useDateRange } from "@/contexts/DateRangeContext";
+import BudgetCard from "@/components/Dashboard/BudgetCard";
+import { CashflowChartUI } from "@/components/Dashboard/CashflowChartUI";
+import { PageHeader } from "@/components/Layout/PageHeader";
 import { api } from "@/convex/_generated/api";
 import { calculateBudget } from "@/lib/budgetCalculations";
-import { filterTransactionsByDateRange } from "@/lib/transactionFilters";
 import { useQuery } from "convex-helpers/react/cache";
 
 export default function Dashboard() {
-  const { selectedDateRange } = useDateRange();
-  const projects = useQuery(api.projects.queries.getAllProjects);
-  const allTransactions = useQuery(
-    api.transactions.queries.getAllTransactions,
-    {},
-  );
-
-  const filteredTransactions = filterTransactionsByDateRange(
-    allTransactions,
-    selectedDateRange,
-  );
-
-  const budgets = calculateBudget(allTransactions ?? []);
+  const transactions = useQuery(api.transactions.queries.getAllTransactions, {});
+  const { currentBalance, expectedIncome, expectedExpenses, availableBudget } = calculateBudget(transactions ?? []);
 
   return (
-    <DashboardUI
-      projects={projects ?? []}
-      transactions={filteredTransactions ?? []}
-      budgets={budgets}
-    />
+    <div>
+      <PageHeader title="Dashboard" showRangeCalendar />
+      <div
+        className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6"
+        id="tour-budget-cards"
+      >
+        <BudgetCard
+          title="Kontostand"
+          amount={currentBalance}
+          description="Verfügbarer Betrag auf dem Konto"
+        />
+        <BudgetCard
+          title="Budget für Ausgaben"
+          amount={availableBudget}
+          description="Kontostand + geplante Einnahmen - geplante Ausgaben"
+        />
+        <BudgetCard
+          title="Muss noch bezahlt werden"
+          amount={expectedExpenses}
+          description="Geplante Ausgaben (müssen noch nicht bezahlt werden)"
+        />
+        <BudgetCard
+          title="Kommt noch rein"
+          amount={expectedIncome}
+          description="Geplante Einnahmen die noch nicht überwiesen wurden"
+        />
+      </div>
+      <div
+        className="flex flex-col lg:flex-row w-full gap-4 lg:gap-6 mt-4 lg:mt-6"
+        id="tour-chart"
+      >
+        <CashflowChartUI />
+      </div>
+    </div>
   );
 }
